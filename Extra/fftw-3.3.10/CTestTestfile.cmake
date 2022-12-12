2eb608c911efaca0b73272fde63bad8338a87d3a
dwsdolce <47837352+dwsdolce@users.noreply.github.com>
2022-12-11 21:05:53 -0800

    Use CMake for fftw
    Changed to use CMake for fftw within Visual Studio 2020

--- Extra/fftw-3.3.10/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Users/dws/src/Tartini/tartini1.3.0/Extra/fftw-3.3.10
-# Build directory: D:/Users/dws/src/Tartini/tartini1.3.0/Extra/fftw-3.3.10
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
